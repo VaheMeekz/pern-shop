65ef9463e6e0281ef29ab8e399d3d2519b55e059
Vahe <vaheemkrtchyan@gmail.com>
2021-12-26 11:39:32 +0400

    models, server test working!

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require("express")
 const sequelize = require("./db")
+const models = require("./models/models")
+const cors = require("cors")
 const PORT = process.env.PORT || 5000
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
+//server test
+// app.get('/',(req, res)=>{
+//     res.status(200).json({message:"working! "})
+// })
 
 const start = async () => {
     try {
